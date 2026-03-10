7aaaa361cd5a07411c6fdfa3d55ea91b6a685433
chenguanxi111 <19823371291@163.com>
2026-03-10 21:22:32 +0800

    初始化 Vue3 项目，第一次提交

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+    function text(){
+alert('这是主要按钮')
+    }
+
+
+</script>
+
+<template>
+  <div>
+    <h1>Element Plus 测试</h1>
+    <el-button type="primary" @click="text">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+    <el-button type="warning">警告按钮</el-button>
+    <el-button type="danger">危险按钮</el-button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
