238354067843b94f230367ff3a802dd66cf2ce99
chenguanxi111 <19823371291@163.com>
2026-03-11 10:10:27 +0800

    添加累加按钮

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
-
+import { ref } from 'vue'
+const a = ref(0)
     function text(){
 alert('这是主要按钮')
     }
     function text1(){
       alert('这是成功按钮')
+    }
+    
+    function count (){
+      a.value++
     }
 
 
@@ -16,7 +21,8 @@ alert('这是主要按钮')
     <el-button type="primary" @click="text">主要按钮</el-button>
     <el-button type="success" @click="text1">成功按钮</el-button>
     <el-button type="warning">警告按钮</el-button>
-    <el-button type="danger">危险按钮</el-button>
+    <h1>{{ a }}</h1>
+    <el-button type="danger" @click="count">累加按钮</el-button>
   </div>
 </template>
 
