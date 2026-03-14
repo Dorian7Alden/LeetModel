223287e302c8049b4cfd0f493a5f398aa82cdde5
chenguanxi111 <19823371291@163.com>
2026-03-15 00:21:47 +0800

    更新

--- LeetModel-vue/src/router/index.js
+++ LeetModel-vue/src/router/index.js
@@ -57,6 +57,7 @@ const routes = [
       },
 
       {
+        path: "problem/:id",
         name: "ProblemDetail",
         component: () => import("../views/problem/ProblemDetail.vue"),
       },
