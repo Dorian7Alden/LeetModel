25772c1f1ab6e680a0e0e6c5e531b681f8bdb02b
Seniorsy <seniorsy.kualk@gmail.com>
2026-03-15 11:23:24 +0800

    feat(backend): 新增了查询最近赛事的接口
    - 后端新增了全局时间格式配置，yyyy-MM-dd HH:mm:ss
    - 新增了competition实体
    - 新增了Controller,Service,Mapper

---
+++ LeetModel-backend/src/main/java/com/senior/leetmodelbackend/mapper/CompetitionMapper.java
@@ -0,0 +1,17 @@
+package com.senior.leetmodelbackend.mapper;
+
+import com.senior.leetmodelbackend.pojo.Competition;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface CompetitionMapper {
+
+    @Select("select * from competition")
+    List<Competition> getAllCompetition();
+
+    @Select("select * from competition order by create_time desc limit 3")
+    List<Competition> getCompetitionsLatest3();
+}
