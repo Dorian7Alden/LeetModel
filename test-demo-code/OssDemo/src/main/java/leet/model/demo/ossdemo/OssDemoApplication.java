867b756b04ff296e1e9899c0caefc6d516f15eed
Kualk4410 <seniorsy.kualk@gmail.com>
2026-04-14 03:22:05 +0800

    demo(oss): 新增了后端上传文件的demo

---
+++ test-demo-code/OssDemo/src/main/java/leet/model/demo/ossdemo/OssDemoApplication.java
@@ -0,0 +1,13 @@
+package leet.model.demo.ossdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OssDemoApplication.class, args);
+    }
+
+}
