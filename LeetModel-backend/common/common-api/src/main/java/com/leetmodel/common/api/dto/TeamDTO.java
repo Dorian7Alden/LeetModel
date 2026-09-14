0d2b0942f449457e2c029e19205f36bc56492d05
Dorian7Alden <Dorian7Alden@gmail.com>
2026-09-14 08:47:08 +0800

    fix(api): 保证运营标识无精度损失

--- LeetModel-backend/common/common-api/src/main/java/com/leetmodel/common/api/dto/TeamDTO.java
+++ LeetModel-backend/common/common-api/src/main/java/com/leetmodel/common/api/dto/TeamDTO.java
@@ -1,5 +1,7 @@
 package com.leetmodel.common.api.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,11 +14,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class TeamDTO {
 
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
     private String name;
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long leaderId;
     private Integer status;
     private Integer memberCount;
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long problemId;
     private String practiceStatus;
     private java.time.LocalDateTime startedAt;
